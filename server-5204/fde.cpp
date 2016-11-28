364bc49c850832e9491f85594bc3c2aeb3f75604
TJ Corley <tjcorley@vt.edu>
2016-11-28 10:35:45 -0500

    Get server code compiling again

--- server-5204/fde.cpp
+++ server-5204/fde.cpp
@@ -1,12 +1,13 @@
 /*
-Copyright (c) 2012-2014 The SSDB Authors. All rights reserved.
-Use of this source code is governed by a BSD-style license that can be
-found in the LICENSE file.
-*/
+   Copyright (c) 2012-2014 The SSDB Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+ */
 #include "fde.h"
 
-struct Fdevent* Fdevents::get_fde(int fd){
-	while((int)events.size() <= fd){
+struct Fdevent* Fdevents::get_fde(int fd)
+{
+	while ((int)events.size() <= fd) {
 		struct Fdevent *fde = new Fdevent();
 		fde->fd = events.size();
 		fde->s_flags = FDEVENT_NONE;
@@ -16,3 +17,9 @@ struct Fdevent* Fdevents::get_fde(int fd){
 	}
 	return events[fd];
 }
+
+#ifdef HAVE_EPOLL
+#include "fde_epoll.cpp"
+#else
+#include "fde_select.cpp"
+#endif
