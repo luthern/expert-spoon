f709233c2550adf7a1cf6846f154fde859e2772e
TJ Corley <tjcorley@vt.edu>
2016-12-10 17:32:27 -0500

    update client

--- testclient/pyclient.py
+++ testclient/pyclient.py
@@ -5,14 +5,13 @@ import time
 
 
 class KVClient(asyncore.dispatcher):
-    def __init__(self, host, file_name, total_to_write):
+    def __init__(self, host, buffer_, total_to_write):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((host, 80))
-        self.file_name = file_name
-        self.file_pointer = open(file_name, 'r')
-        self.total_writes = 0
+        self.packet_buffer = buffer_
         self.total_to_write = total_to_write
+        self.total_writes = 0
         self.is_writable = True
         self.end_time = 0
 
@@ -30,25 +29,11 @@ class KVClient(asyncore.dispatcher):
         return self.is_writable
 
     def handle_write(self):
-        line = self.file_pointer.readline()
-        if not line:
+        if self.total_writes == self.total_to_write:
             self.is_writable = False
             return
-        line = line.split(':')
-        operation = int(line[0])
-        key = line[1]
-        if len(line[2]) != 32:
-            value = '\x00' * 32
-        else:
-            value = line[2]
-
-        keep_alive = 0x1
-        if self.total_writes == self.total_to_write - 1:
-            keep_alive = 0x0
-        buffer = chr(operation) + chr(keep_alive) + key + value
-        self.send(buffer)
+        self.send(self.packet_buffer[self.total_writes])
         self.total_writes += 1
-        # print(self.total_writes)
 
     def get_end_time(self):
         return self.end_time
@@ -57,10 +42,36 @@ class KVClient(asyncore.dispatcher):
 filename = sys.argv[1]
 number_of_messages = int(sys.argv[2])
 
-client = KVClient('192.168.0.151', filename, number_of_messages)
+file_pointer = open(filename, 'r')
+lines = file_pointer.read().split('\n')
+index = 0
+packet_buffer = []
+for line in lines:
+    if not line:
+        break
+    line = line.split(':')
+    operation = int(line[0])
+    key = line[1]
+    if len(line[2]) != 32:
+        value = '\x00' * 32
+    else:
+        value = line[2]
+    keep_alive = 0x1
+    if index == number_of_messages - 1:
+        keep_alive = 0x0
+    packet_buffer.append(chr(operation) + chr(keep_alive) + key + value)
+    index += 1
+file_pointer.close()
+clients = []
+
+for c in range(0, 1000):
+    clients.append(KVClient('192.168.0.151', packet_buffer,
+                            number_of_messages))
+
 start = time.time()
 asyncore.loop()
-end = client.get_end_time()
+end = clients[0].get_end_time()
 
+print("total_writes ", clients[100].total_writes)
 print("total seconds ", end - start)
 print("requests/second ", number_of_messages / (end - start))
